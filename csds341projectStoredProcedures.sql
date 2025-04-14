7dc957a6dbc6dc6f354472374c09b3c34431eb68
Bailey Dalton <bmd81@case.edu>
2025-04-14 18:42:09 -0400

    Implements the help command

--- csds341projectStoredProcedures.sql
+++ csds341projectStoredProcedures.sql
@@ -38,6 +38,39 @@ BEGIN
 	SELECT @new_college_name = colName FROM college WHERE colID = @new_colID;
 END;
 
+CREATE PROCEDURE replaceCoach
+    @tID INT,
+    @new_coaID INT,
+	@old_coach_name varchar(40) OUTPUT,
+	@new_coach_name varchar(40) OUTPUT,
+	@team_college_name varchar(50) OUTPUT
+AS
+BEGIN
+    DECLARE @old_coaID INT;
+
+    -- Get the coach we are firing
+    SELECT @old_coaID = coaID
+    FROM team
+    WHERE tID = @tID;
+
+    -- Update the team's coach
+    UPDATE team
+    SET coaID = @new_coaID
+    WHERE tID = @tID;
+
+    -- Increment jobNum for newly hired coach
+    UPDATE coach
+    SET jobNum = COALESCE(jobNum, 0) + 1
+    WHERE coaID = @new_coaID;
+
+    -- Return player's name and new school to output
+	SELECT @old_coach_name = fname + ' ' + lname  FROM coach WHERE coaID = @old_coaID;
+	SELECT @new_coach_name = fname + ' ' + lname  FROM coach WHERE coaID = @new_coaID;
+	SELECT @team_college_name = colName FROM college WHERE colID = (
+		SELECT colID FROM team WHERE tID = @tID
+	);
+END;
 
 -- Give dbuser ability to execute this stored procedures
 GRANT EXECUTE ON OBJECT::transferPlayer TO dbuser;
+GRANT EXECUTE ON OBJECT::replaceCoach TO dbuser;
