a9bd0a83a3f8e52694f0ec14607a8bfe3473ca3c
acm0212 <acm265@case.edu>
2025-04-17 10:47:54 -0400

    adds removePlayer and expandSponsorship stored procedures

--- csds341projectStoredProcedures.sql
+++ csds341projectStoredProcedures.sql
@@ -71,6 +71,91 @@ BEGIN
 	);
 END;
 
+CREATE PROCEDURE removePlayer
+	@pID int,
+	@player_name varchar (40) output,
+	@college_name varchar (50) output,
+	@updated_numStudents int
+AS
+BEGIN
+	DECLARE @colID int;
+
+	--get player's name and current school
+	SELECT
+		@player_name = p.fname + ' ' + p.lname,
+		@colID = t.colID
+	FROM player as p
+	JOIN team as t
+	ON p.tID = t.tID
+	WHERE p.pID = @pID;
+
+	--get college name
+	SELECT @college_name = colName
+	FROM college
+	WHERE colID = @colID;
+
+	--decrement that college's numStudents
+	UPDATE college
+	SET numStudents = numStudents -1
+	WHERE colID = @colID;
+
+	--show updated number
+	SELECT @updated_numStudents = numStudents
+	FROM college
+	WHERE colID = @colID;
+
+	--delete player
+	DELETE FROM player
+	WHERE pID = @pID;
+
+	-- select statement to summarize changes
+	SELECT 
+		@player_name as RemovedPlayer,
+		@college_name as College,
+		@updated_numStudents as RemainingStudents;
+END;
+
+CREATE PROCEDURE expandSponsorship
+	@sID int,
+	@additionalContribution int,
+	@new_tID int,
+	@sponsor_name varchar(50) output,
+	@new_contribution int output
+AS
+BEGIN
+	--increase sponsor contribution
+	UPDATE sponsorship
+	SET contribution = contribution +@additionalContribution
+	WHERE sID = @sID;
+
+	--sponsor name and new total contribution
+	SELECT 
+		@sponsor_name = sName,
+		@new_contribution = contribution
+	FROM sponsorship
+	WHERE sID = @sID;
+
+	--Link sponsor to new team
+	INSERT INTO teamSponsors (tID, sID)
+	VALUES (@new_tID, @sID);
+
+	--summary of what happened
+	SELECT 
+		@sponsor_name as Sponsor,
+		@new_contribution as TotalContribution;
+
+	--list all teams the sponsor now supports
+	SELECT 
+		ts.tID,
+		t.tName
+	FROM teamSponsors as ts
+	JOIN team as t
+		ON ts.tID = t.tID
+	WHERE ts.sID = @sID;
+END;
+
 -- Give dbuser ability to execute this stored procedures
 GRANT EXECUTE ON OBJECT::transferPlayer TO dbuser;
 GRANT EXECUTE ON OBJECT::replaceCoach TO dbuser;
+GRANT EXECUTE ON OBJECT::removePlayer TO dbuser;
+GRANT EXECUTE ON OBJECT::expandSponsorship TO dbuser;
